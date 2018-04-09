60582eae694b05074963b29888ca74b72ddb2056
Nikita Kirillov <nik2010spartak@yandex.ru>
2018-04-10 01:14:28 +0300

    Добавил конструкторы и 2 метода: активационной функции и расчета состояния

--- index.php
+++ index.php
@@ -8,14 +8,51 @@
     <h1>Нейросеть деффазификации</h1> 
     
       <?php
-          
+        define("DEFAULT_W0", 0.25);
+        define("DEFAULT_W", 0.2);
+        define("DEFAULT_ALF", 1);
+        define("KOL_ENTER", 10);
     
+        class Neuron  {
+            private $w = []; 
+            private $w0 = DEFAULT_W0;             
+            private $alf = DEFAULT_ALF; 
+            private $sigm;
+            private $s;
+            private $x = [];
+            private $y;
+            
+            function __construct() {
+                for ($i = 0; $i < KOL_ENTER; $i++) {
+                    array_push($this->w, DEFAULT_W);
+                }
+            }
+            
+            function __construct1($count) {
+                for ($i = 0; $i < $count; $i++) {
+                    array_push($this->w, DEFAULT_W);
+                }
+            }
+            
+            public function Status() {
+                $this->s = 0;
+                for ($i = 0; $i < count($this->x); $i++) {
+                    $this->s += $this->w[i] * $this->x[i];
+                }
+                $this->s += $this->w0;
+                echo $this->s;
+                echo "hello";
+            }
+            
+            public function funk() {
+                $this->y = 1 / (exp(-($this->alf)*($this->S)) + 1);
+            }
+        }
+        
+        $test = new Neuron();
+//        $test->Status();
     
-    
-    
-    
-    
-    
+        
     
       ?>
 </body>
