4c6bdaaf9a8464c77ceb8309235ea46f198323cd
Nikita Kirillov <nik2010spartak@yandex.ru>
2018-04-28 15:39:15 +0300

    Подключил библиотеку для графика

--- index.php
+++ index.php
@@ -5,26 +5,27 @@
     <title>Lessons</title>
     <link rel="stylesheet" href="styles/style.css">
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="scripts/plot/jquery.jqplot.css" />
     <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
 </head>
 <body>
-   <div class="wrapper">
-       <div class="header">
-           <h1>Нейросетевой дефаззификатор</h1>
-           <p>Создан при поддержке кафедры ИКТ РХТУ им. Д. И. Менделеева</p>
-       </div>
-       <div class="main">
-           <div class="sidebar">
-               <div class="menu-title">
-                   <i class="fas fa-cog"></i>
-                   <span>Параметры</span>
-               </div>
-               <div class="description">
-                   <p>
-                       Задайте координаты вершин (x) дефаззификации и соответствующие степени принадлежности (&alpha;).
-                   </p>        
-               </div>                      
-               <form class="calculate-form" method="post" action="">
+    <div class="wrapper">
+        <div class="header">
+            <h1>Нейросетевой дефаззификатор</h1>
+            <p>Создан при поддержке кафедры ИКТ РХТУ им. Д. И. Менделеева</p>
+        </div>
+        <div class="main">
+            <div class="sidebar">
+                <div class="menu-title">
+                    <i class="fas fa-cog"></i>
+                    <span>Параметры</span>
+                </div>
+                <div class="description">
+                    <p>
+                        Задайте координаты вершин (x) дефаззификации и соответствующие степени принадлежности (&alpha;).
+                    </p>        
+                </div>                      
+                <form class="calculate-form" method="post" action="">
                    <?php 
                         for($i = 0; $i < 5; $i++){
                             print '<div class="row">
@@ -38,15 +39,14 @@
                                        </div>    
                                    </div>';
                         }
-                   
                    ?>
-                   
-                   <div class="row">
-                       <button type="submit">Рассчитать</button>
-                   </div>  
+                    <div class="row">
+                        <button type="submit">Рассчитать</button>
+                    </div>  
                 </form>
-           </div>
-           <div class="content">
+            </div>
+            <div class="content">
+                <div class="chart"></div>
     
       <?php
         if(isset($_POST['0']) && isset($_POST['1'])) {       
@@ -170,9 +170,18 @@
         }
       ?>
       
-       </div>
-   </div>
-</div>
-    
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.9/jquery.jqplot.min.js"></script>  
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.canvasTextRenderer.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.canvasAxisLabelRenderer.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.canvasAxisTickRenderer.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.canvasOverlay.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.highlighter.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.cursor.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.categoryAxisRenderer.js"></script>
+    <script type="text/javascript" src="scripts/plot/plugins/jqplot.dateAxisRenderer.js"></script>    
 </body>
 </html>
